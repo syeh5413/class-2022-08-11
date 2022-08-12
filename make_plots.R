79ffc6b04ee9607f68185e9452dca348c1b3d19f
syeh5413 <syeh5413@stu.d214.org>
2022-08-11 20:00:38 -0500

    better pages

--- make_plots.R
+++ make_plots.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(knitr)
 
 x <- read_rds(file = "clean_data.rds")
 
@@ -14,3 +15,19 @@ plot_1 <- x |>
        subtitle = "There is no relationship between population and economic connectedness in US counties.")
 
 write_rds(plot_1, "plot_1.rds")
+
+plot_2 <- x |> 
+  group_by(state) |> 
+  summarise(pop_state = sum(pop2018),
+            avg_ec = mean(ec_county)) |> 
+  ggplot(aes(x = pop_state, y = avg_ec)) +
+  geom_point() +
+  geom_smooth(method = "lm", formula = y ~ x, se = FALSE) +
+  scale_x_log10(breaks = c(1000000, 10000000),
+                labels = c("1,000,0000", "10,000,000")) +
+  labs(y = "Average County Economic Connectedness",
+       x = "State Population",
+       title = "Economic Connectedness and Population in US States",
+       subtitle = "Biggger states have counties with lower average economic connectedness.")
+
+write_rds(plot_2, "plot_2.rds")
